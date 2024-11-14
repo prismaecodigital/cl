bbd9525611ceaa4f93971927408cee19ae48bd6a
k1adi <rizkiadinugroho3@gmail.com>
2024-11-14 14:41:18 +0700

    display room table

--- resources/js/Pages/Room/Index.jsx
+++ resources/js/Pages/Room/Index.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import createColumn from './data';
 import DashboardLayout from '@/Layouts/DashboardLayout';
+import Breadcrumb from '@/Components/Breadcrumb/Breadcrumb';
+import MyTable from '@/Components/Table/MyTable';
 
-function Index({ rooms }) {
-  console.log(rooms);
+function Index({auth, rooms }) {
+  const columns = createColumn();
+  const breadcrumb = [
+    { link: route('dashboard'), text: 'Dashboard' },
+		{ link: '#', text: 'Database' },
+  ];
   
   return (
     <div className='content-box'>
-      <h1 className='text--title'>Room</h1>
+      <Breadcrumb pageName='Room' prevPage={breadcrumb} />
+
+      <MyTable data={rooms.data} columns={columns} />
     </div>
-  );
+  ); 
 }
 
 Index.layout = page => (
