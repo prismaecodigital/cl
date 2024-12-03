94a891de5ca8a31c0dafed3bf2dbf2d9066007c5
k1adi <rizkiadinugroho3@gmail.com>
2024-12-03 16:15:31 +0700

    build production

---
+++ public/build/assets/Dashboard-5811e4e6.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-17e9f1c4.js";import{D as o}from"./DashboardLayout-2d1d7f91.js";import"./createLucideIcon-29b2380b.js";import"./mails-e875ca7a.js";function r(){return t.jsx("div",{className:"content-box",children:t.jsx("h1",{className:"text--title",children:"Dashboard"})})}r.layout=a=>t.jsx(o,{title:"Dashboard",children:a});export{r as default};
